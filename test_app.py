2991a4c0449ab4f3b1ddb32bc5f1c4636feaf252
Martin Bozhilov <marogo142005@gmail.com>
2024-03-26 11:30:50 +0200

    [REMOVE] healthcheck

--- test_app.py
+++ test_app.py
@@ -7,12 +7,6 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_health_check(client):
-    """Test the health check route."""
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == {'message': 'Server is up and running'}
-
 def test_get_books(client):
     """Test retrieving all books."""
     response = client.get('/books')
